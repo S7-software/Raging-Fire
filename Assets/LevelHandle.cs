cc6555e935671b58a26237f90cec5a34feb259ff
Sinan TEKİN <37111233+sinan73k1n@users.noreply.github.com>
2022-10-30 00:44:35 +0300

    UI_LEVELS

---
+++ Assets/LevelHandle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelHandle : MonoBehaviour
+{
+    [SerializeField] Color _on, _off;
+    [SerializeField] TMP_Text _txtLevelNumber;
+    [SerializeField] Image[] _stars,_starsSocked;
+    [SerializeField] Button _myBtn;
+    int _index;
+
+    public void SetButton(int level,int stars,bool aktif)
+    {
+        _index = level;
+        _txtLevelNumber.text = "" + _index;
+        _txtLevelNumber.color= aktif ? _on : _off;
+        _stars[0].enabled = stars > 0;
+        _stars[1].enabled = stars > 1;
+        _stars[2].enabled = stars > 2;
+        foreach (var item in _starsSocked)
+        {
+            item.color = aktif ? _on : _off;
+        }
+        _myBtn.interactable = aktif;
+        _myBtn.image.color = aktif ? _on : _off;
+        _myBtn.onClick.AddListener(() => { FindObjectOfType<UI_LEVELS>().GoToLevel(_index); });
+    }
+   
+}
---
+++ Assets/UI_LEVELS.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_LEVELS : MonoBehaviour
+{
+    [SerializeField] Image _panel;
+    [SerializeField] CanvasGroup _canvasGroup;
+    [SerializeField] GameObject _level;
+    [SerializeField] Transform _trasformTable,_transformContent;
+    [SerializeField] float _duration = 0.4f;
+    [SerializeField] Button _btnExit;
+
+    private void Awake()
+    {
+        STUIAnim.SetAwake(_panel, _canvasGroup);
+    }
+    private void Start()
+    {
+        STUIAnim.In(_panel, _canvasGroup, _trasformTable, _duration);
+
+        for (int i = 1; i < 25; i++)
+        {
+            GameObject gameObject = Instantiate(_level,_transformContent);
+          
+            gameObject.GetComponent<LevelHandle>().SetButton(i, Random.Range(1, 4), true);
+        }
+        for (int i = 25; i < 30; i++)
+        {
+            GameObject gameObject = Instantiate(_level, _transformContent);
+
+            gameObject.GetComponent<LevelHandle>().SetButton(i, 0, false);
+        }
+
+        _btnExit.onClick.AddListener(() => { STUIAnim.Out(_panel, _canvasGroup, _trasformTable, _duration, gameObject); Invoke("Menu", _duration - 0.1f); });
+    }
+
+
+    void Menu() { GameManager.instantiate.CloneUI_MAIN_MENU(); }
+    public void GoToLevel(int hangi)
+    {
+        SceneManager.LoadScene(hangi);
+    }
+}
